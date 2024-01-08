13fe94679347c9275dcf4c5da5081db35a3342c3
eugene.kopyl <eugene.kopyl@vrpconsulting.com>
2024-01-08 08:35:00 +0300

    feat: (dev_next) parts names

--- constants/parts.js
+++ constants/parts.js
@@ -1,27 +1,27 @@
 export const partsItems = [
     {
         title: 'Ролик системы когель',
-        image: '/images/parts/ролик система когель.jpg',
+        image: '/images/parts/ролик-система-когель.jpg',
     },
     {
         title: 'Ролик системы KOGEL 629-K',
-        image: '/images/parts/РОЛИК СИСТЕМЫ KOGEL 629-K.jpg',
+        image: '/images/parts/РОЛИК-СИСТЕМЫ-KOGEL-629-K.jpg',
     },
     {
         title: 'Ролик системы SCHMITZ 608-ST',
-        image: '/images/parts/РОЛИК СИСТЕМЫ SCHMITZ 608-ST.jpg',
+        image: '/images/parts/РОЛИК-СИСТЕМЫ-SCHMITZ-608-ST.jpg',
     },
     {
         title: 'Ролик системы SESAM 609-S',
-        image: '/images/parts/РОЛИК СИСТЕМЫ SESAM 609-S.jpg',
+        image: '/images/parts/РОЛИК-СИСТЕМЫ-SESAM-609-S.jpg',
     },
     {
         title: 'Ролик системы TSE 609-ELP',
-        image: '/images/parts/РОЛИК СИСТЕМЫ TSE 609-ELP.jpg',
+        image: '/images/parts/РОЛИК-СИСТЕМЫ-TSE-609-ELP.jpg',
     },
     {
         title: 'Ролик системы TSE 609-T',
-        image: '/images/parts/РОЛИК СИСТЕМЫ TSE 609-T.jpg',
+        image: '/images/parts/РОЛИК-СИСТЕМЫ-TSE-609-T.jpg',
     },
     {
         title: 'Ролик-EDSHA-1',
@@ -33,62 +33,62 @@ export const partsItems = [
     },
     {
         title: 'Штанга натяжения тента',
-        image: '/images/parts/ШТАНГА НАТЯЖЕНИЯ ТЕНТА.gif',
+        image: '/images/parts/ШТАНГА-НАТЯЖЕНИЯ-ТЕНТА.gif',
     },
     {
         title: 'Крючок оцинкованный',
-        image: '/images/parts/КРЮЧОК ОЦИНКОВАННЫЙ.png',
+        image: '/images/parts/КРЮЧОК-ОЦИНКОВАННЫЙ.png',
     },
     {
         title: 'Крючок пластмассовый',
-        image: '/images/parts/КРЮЧОК ПЛАСТМАССОВЫЙ.jpg',
+        image: '/images/parts/КРЮЧОК-ПЛАСТМАССОВЫЙ.jpg',
     },
     {
         title: 'Наконечник на трос TIR',
-        image: '/images/parts/НАКОНЕЧНИК НА ТРОС TIR.jpg',
+        image: '/images/parts/НАКОНЕЧНИК-НА-ТРОС-TIR.jpg',
     },
     {
         title: 'подкладка пластиковая A-51',
-        image: '/images/parts/подкладка пластиковая а 51.png',
+        image: '/images/parts/подкладка-пластиковая-а-51.png',
     },
     {
         title: 'подкладка пластиковая A-20',
-        image: '/images/parts/ПОДКЛАДКА ПЛАСТИКОВАЯ А-20.png',
+        image: '/images/parts/ПОДКЛАДКА-ПЛАСТИКОВАЯ-А-20.png',
     },
     {
         title: 'подкладка пластиковая A-34',
-        image: '/images/parts/ПОДКЛАДКА ПЛАСТИКОВАЯ А-34.png',
+        image: '/images/parts/ПОДКЛАДКА-ПЛАСТИКОВАЯ-А-34.png',
     },
     {
         title: 'подкладка пластиковая',
-        image: '/images/parts/Подкладка пластиковая.png',
+        image: '/images/parts/Подкладка-пластиковая.png',
     },
     {
         title: 'Скоба бортавая колебательная вертикальная',
-        image: '/images/parts/СКОБА БОРТОВАЯ КОЛЕБАТЕЛЬНАЯ ВЕРТИКАЛЬНАЯ.webp',
+        image: '/images/parts/СКОБА-БОРТОВАЯ-КОЛЕБАТЕЛЬНАЯ-ВЕРТИКАЛЬНАЯ.webp',
     },
     {
         title: 'Скоба бортавая круглая',
-        image: '/images/parts/СКОБА БОРТОВАЯ КРУГЛАЯ.webp',
+        image: '/images/parts/СКОБА-БОРТОВАЯ-КРУГЛАЯ.webp',
     },
     {
         title: 'Скоба бортавая на квадратном основании',
-        image: '/images/parts/СКОБА БОРТОВАЯ НА КВАДРАТНОМ ОСНОВАНИИ.webp',
+        image: '/images/parts/СКОБА-БОРТОВАЯ-НА-КВАДРАТНОМ-ОСНОВАНИИ.webp',
     },
     {
         title: 'Скоба загнутая французская',
-        image: '/images/parts/СКОБА ЗАГНУТАЯ ФРАНЦУЗСКАЯ.jpg',
+        image: '/images/parts/СКОБА-ЗАГНУТАЯ-ФРАНЦУЗСКАЯ.jpg',
     },
     {
         title: 'Скоба поворотная',
-        image: '/images/parts/СКОБА ПОВОРОТНАЯ.jpg',
+        image: '/images/parts/СКОБА-ПОВОРОТНАЯ.jpg',
     },
     {
         title: 'Трос TIR с наконечником',
-        image: '/images/parts/ТРОС TIR с наконечником.png',
+        image: '/images/parts/ТРОС-TIR-с-наконечником.png',
     },
     {
         title: 'Эспандер черный (полипропиленовый)',
-        image: '/images/parts/ЭСПАНДЕР ЧЁРНЫЙ (ПОЛИПРОПИЛЕНОВЫЙ).jpg',
+        image: '/images/parts/ЭСПАНДЕР-ЧЁРНЫЙ-(ПОЛИПРОПИЛЕНОВЫЙ).jpg',
     },
 ];
